7d9951f3d8f486fcaf7d3075e5606413e7ae3be6
se0413 <tldms7133@naver.com>
2025-08-13 14:11:25 +0900

    250813_1

--- goeunmirai/js/main.js
+++ goeunmirai/js/main.js
@@ -1,22 +1,60 @@
 $(document).ready(function(){
 
-    const visual_swiper = new Swiper('.visual .swiper', { 
+    // const visual_swiper = new Swiper('.visual .swiper', { 
 
-        effect: "fade", 
-        autoplay: {  
-            delay: 5000,
-            disableOnInteraction: true,
-        },
+    //     effect: "fade", 
+    //     autoplay: {  
+    //         delay: 5000,
+    //         disableOnInteraction: true,
+    //     },
 
-        loop: true,  
+    //     loop: true,  
 
-        pagination: {  
-            el: '.visual .paging', 
-            clickable: true,  
-        },
+    //     pagination: {  
+    //         el: '.visual .paging', 
+    //         clickable: true,  
+    //     },
     
 
+    // });
+
+    const visual_swiper = new Swiper(".visual .swiper", {
+        loop: true,
+        effect: "fade",
+        autoplay: {
+            delay: 5000, // 5초마다 슬라이드
+            disableOnInteraction: false,
+        },
+        on: {
+            init: function () {
+            document.querySelector(".total").textContent = formatNumber(this.slides.length - this.loopedSlides * 2);
+            updateNumber(this.realIndex + 1);
+            resetProgress();
+            },
+            slideChangeTransitionStart: function () {
+            updateNumber(this.realIndex + 1);
+            resetProgress();
+            },
+        },
     });
 
+    function formatNumber(num) {
+        return num.toString().padStart(2, "0");
+    }
+
+    function updateNumber(current) {
+        document.querySelector(".current").textContent = formatNumber(current);
+    }
+
+    function resetProgress() {
+        const progressBar = document.querySelector(".progress-bar");
+        progressBar.style.transition = "none";
+        progressBar.style.width = "0%";
+
+        setTimeout(() => {
+            progressBar.style.transition = "width 5s linear";
+            progressBar.style.width = "100%";
+        }, 50);
+    }
 
 })
