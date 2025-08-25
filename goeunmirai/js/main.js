bfae3fe458c1d961bb5d5d81d2fb716b6d32947e
se0413 <tldms7133@naver.com>
2025-08-25 16:21:16 +0900

    250825_3

--- goeunmirai/js/main.js
+++ goeunmirai/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         loop: true,
         effect: "fade",
         autoplay: {
-            delay: 5000, // 5초마다 슬라이드
+            delay: 5000,
             disableOnInteraction: false,
         },
         on: {
@@ -40,17 +40,6 @@ $(document).ready(function(){
     }
 
 
-    /*
-        1. tab버튼을 클릭(.cts_history .tap_area .tap_btn ul li)하면 active클래스를 줘야함, 다른건 삭제
-        2. 클릭한 li만 aria-selected="true" 나머지는 aria-selected="false"
-        3. 클릭한 li에서 aria-controls 값을 가져와서 하단 콘텐츠(.cts_history .tap_area .tap_cnt div[role="tabpanel"]) 
-            중에서 같은 이름의 id를 가진 요소에만 active클래스를 줌
-        
-            find 명령으로 id가 aria-controls 값과 같은 요소를 찾아야함
-            find는 하위요소를 검색하는 기능
-            선택자가 tabpanel을 직접 선택하는게 아니라 그 부모요소를 선택해서 하위요소를 검색하게 해야함
-    */
-   
     $('.team, .signature').each(function(){
         let $group = $(this);
         let $tab_btn = $group.find('.tap_area .tap_btn ul li');
@@ -69,7 +58,25 @@ $(document).ready(function(){
         });
     });
 
-     
+    const ba_swiper = new Swiper(".ba .swiper", {
+        loop: true,
+        effect: "fade",
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        on: {
+            init: function () {
+            document.querySelector(".total").textContent = formatNumber(this.slides.length - this.loopedSlides * 2);
+            updateNumber(this.realIndex + 1);
+            resetProgress();
+            },
+            slideChangeTransitionStart: function () {
+            updateNumber(this.realIndex + 1);
+            resetProgress();
+            },
+        },
+    });     
 
      
 
