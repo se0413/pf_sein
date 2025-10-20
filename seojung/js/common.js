23f214a7c3f6af0ad1050681f4b859e0bcd9991d
se0413 <tldms7133@naver.com>
2025-10-20 16:57:49 +0900

    251020_2

--- seojung/js/common.js
+++ seojung/js/common.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
     })
 
 
-    $('header').on('mouseenter focusin', function(){
+    $('.gnb').on('mouseenter focusin', function(){
         $(this).addClass('fixed')
     })
     $('header').on('mouseleave', function(){
@@ -45,11 +45,22 @@ $(document).ready(function(){
     //header .gnb .gnb_open, header .gnb .gnb_close
     $('header .gnb .gnb_open').on('click', function(e){
         $('header').addClass('menu_open')
+        $('header').removeClass('fixed')
         $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
     })
     $('header .gnb .gnb_close').on('click', function(){
         $('header').removeClass('menu_open')
         $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
+    })
+
+    $('header .gnb .gnb_wrap .depth1 > li').on('click', function(){
+        $('header .gnb .gnb_wrap .depth1 > li').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    $('header .gnb .gnb_wrap').on('mouseleave', function(){
+        $('header .gnb .gnb_wrap .depth1 > li').removeClass('active')
+
     })
 
 })
