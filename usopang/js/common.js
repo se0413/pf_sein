e62d8d2614d511b9dd989d2d0aeaba4dc805bbd4
cat0413 <tldms7133@naver.com>
2025-12-02 10:04:16 +0900

    251202_1

--- usopang/js/common.js
+++ usopang/js/common.js
@@ -76,4 +76,13 @@ $(document).ready(function(){
         $('footer .famillysite').removeClass('open')
         $('footer .famillysite ul').slideUp()
     })
+
+    //cursor
+    $(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
+        $('.cursor').css('left', e.pageX + 'px');
+        $('.cursor').css('top', e.pageY + 'px');
+    });
+    $('.popup').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+        $('.cursor').toggleClass('on');
+    });
 })
