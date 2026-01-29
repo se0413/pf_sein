c702519c5393828e1029dbf1f3955b185a412642
cat0413 <tldms7133@naver.com>
2026-01-29 17:17:48 +0900

    .

--- dongwha/js/main.js
+++ dongwha/js/main.js
@@ -17,112 +17,90 @@ $(document).ready(function(){
 
     });
 
-    const now_swiper = new Swiper('.now .swiper', { 
+    let slogan = $('.tit')
+    let slogan_obj = $('.tit p span')
+    let slogan_rate_s = 0.3
+    let slogan_rate_e = 0.6
+    let slogan_leng = slogan_obj.length
+    let slogan_scroll
+    let slogan_top
+    let slogan_start
+    let slogan_end
+    let slogan_w
+    let scrolling
+    let win_h
 
-        pagination: { 
-            el: '.now .swiper-pagination',
-            clickable: true, 
-            type: 'fraction',
-            renderBullet: function (index, className) {
-                return '<span class="' + className + '">' + (index + 1) + "</span>";
-            },
-        },
+    // 구급상자 애니메이션 변수
+    let kit_img = $('.trust .photo img')
+    let kit_rate_s = 0.4 // 구급상자 애니메이션 시작 지점
+    let kit_rate_e = 0.9 // 구급상자 애니메이션 종료 지점
+    let kit_scroll
+    let kit_scale
+    let kit_translateY
+
+    slogan_ani()
+    kit_ani()
+
+    $(window).scroll(function(){
+        slogan_ani()
+        kit_ani()
+    })
+
+    $(window).resize(function(){
+        slogan_ani()
+        kit_ani()
+    })
+
+    function slogan_ani(){
+        win_h = $(window).height()
+        scrolling = $(window).scrollTop()
+        slogan_top = slogan.offset().top
+        slogan_start = slogan_top - win_h + (win_h * slogan_rate_s)
+        slogan_end = slogan_top + slogan.height() - win_h + (win_h * slogan_rate_e)
+        slogan_scroll = (scrolling - slogan_start) / (slogan_end - slogan_start) * 100
         
-
-        navigation: {
-            nextEl: '.now .btn_next', 
-            prevEl: '.now .btn_prev',  
-        },
-
-    });
-
-
-//tap 기능
-    function initTabMenu(sectionClass) {
-        let tab_btn = $(sectionClass + ' .tap_box ul li') 
-        let tab_name
-        let tab_cnt = $(sectionClass + ' .tap_cnt div[role="tabpanel"]')
-        let tab_cnt_prant = $(sectionClass + ' .tap_cnt')
-
-        tab_btn.on('click', function(){
-            tab_btn.removeClass('active')
-            $(this).addClass('active')
-            tab_btn.attr('aria-selected', 'false')
-            $(this).attr('aria-selected', 'true')
-            
-            tab_name = $(this).attr('aria-controls')
-            tab_name = '#'+ tab_name
-            
-            tab_cnt.removeClass('active')
-            tab_cnt_prant.find(tab_name).addClass('active')
-        })
+        if(slogan_start > scrolling) {
+            slogan_obj.width(0)
+        }else if(slogan_end > scrolling){
+            for(i=0; i<slogan_leng; i++){
+                slogan_w = (slogan_scroll - (100/slogan_leng)*i) * slogan_leng
+                if(slogan_w > 100){
+                    slogan_w = 100
+                }
+                slogan_obj.eq(i).width(slogan_w + '%')
+            }
+        }else{
+            slogan_obj.width('100%')
+        }
     }
 
-    // 실행 (섹션마다 호출)
-    initTabMenu('.cju_board .now')
-    initTabMenu('.notice')  
-    initTabMenu('.sns')
-
-
-    const research_swiper = new Swiper('.research .swiper', { 
-        slidesPerView: 'auto',
-        pagination: { 
-            el: '.research .swiper-pagination',
-            clickable: true, 
-            type: 'fraction',
-            renderBullet: function (index, className) {
-                return '<span class="' + className + '">' + (index + 1) + "</span>";
-            },
-        },
-
-        breakpoints: {
-            640: {    /* 640px 이상일때 적용 */
-                slidesPerView: 1,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-                spaceBetween: 20,
-            },
-        },
-
-        navigation: {
-            nextEl: '.research .btn_next', 
-            prevEl: '.research .btn_prev',  
-        },
-
-    });
-
-    const pride_swiper = new Swiper('.pride .swiper', { 
-
-        pagination: { 
-            el: '.pride .swiper-pagination',
-            clickable: true, 
-            renderBullet: function (index, className) {
-                return '<span class="' + className + '">' + (index + 1) + "</span>";
-            },
-        },
-
-    });
-
-    const sns_swiper = new Swiper('.sns .swiper', { 
-    centeredSlides: true,
-    slidesPerView: 1, 
-    initialSlide: 3,
-        spaceBetween: 16,
-        breakpoints: {
-            1500: {
-                slidesPerView: 5,
-                spaceBetween: 24,
-            },
-            568: {
-                slidesPerView: 3,
-                spaceBetween: 24,
-            },
-        },
-        scrollbar: {
-            el: ".sns .swiper-scrollbar",
-            hide: false,
-            draggable: true,
-            dragSize: 20,
-        },
-
-    });
+    function kit_ani(){
+        win_h = $(window).height()
+        scrolling = $(window).scrollTop()
+        slogan_top = slogan.offset().top
+        
+        let kit_start = slogan_top - win_h + (win_h * kit_rate_s)
+        let kit_end = slogan_top + slogan.height() - win_h + (win_h * kit_rate_e)
+        kit_scroll = (scrolling - kit_start) / (kit_end - kit_start) * 100
+        
+        if(kit_start > scrolling) {
+            // 애니메이션 시작 전
+            kit_scale = 1
+            kit_translateY = 0
+        }else if(kit_end > scrolling){
+            // 애니메이션 진행 중
+            kit_scroll = Math.max(0, Math.min(100, kit_scroll))
+            kit_scale = 1 + (kit_scroll / 100) * 1.5 // 1배에서 2.5배까지 확대
+            kit_translateY = (kit_scroll / 100) * 100 // 100px 아래로 이동
+        }else{
+            // 애니메이션 종료 후
+            kit_scale = 2.5
+            kit_translateY = 100
+        }
+        
+        kit_img.css({
+            'transform': 'scale(' + kit_scale + ') translateY(' + kit_translateY + 'px)'
+        })
+    }
 
 })
