2ca221a843dde4bdf57651802b2e4da4ac5f2706
cat0413 <tldms7133@naver.com>
2026-04-27 15:38:05 +0900

    260427_2

--- nahes/js/common.js
+++ nahes/js/common.js
@@ -41,29 +41,13 @@ $(document).ready(function(){
         scroll_chk()
     })
 
-    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin', function(){
-        if(pc_mobile == 'pc'){
-            $('header').addClass('menu_over')
-            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
-            $(this).addClass('on')
-        }
-    })
-    $('header').on('mouseleave', function(){
-        if(pc_mobile == 'pc'){
-            $('header').removeClass('menu_over')
-            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
-            $(this).removeClass('on')
-        }
-    })
-    
-
     //header .gnb .gnb_open, header .gnb .gnb_close
     $('header .gnb .gnb_open').on('click', function(e){
-        $('header').addClass('menu_open')
+        $('header').addClass('menu_over')
         $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
     })
     $('header .gnb .gnb_close').on('click', function(){
-        $('header').removeClass('menu_open')
+        $('header').removeClass('menu_over')
         $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
     })
 
@@ -77,13 +61,5 @@ $(document).ready(function(){
         $('footer .famillysite ul').slideUp()
     })
 
-    //cursor
-    $(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
-        $('.cursor').css('left', e.pageX + 'px');
-        $('.cursor').css('top', e.pageY + 'px');
-    });
-    $('.c_event').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
-        $('.cursor').toggleClass('on');
-    });
 
 })
