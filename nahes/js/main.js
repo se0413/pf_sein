5a4e5a608d66ca0e0f46705a88c3cd9895a94ff4
cat0413 <tldms7133@naver.com>
2026-04-28 19:01:46 +0900

    260428_1

--- nahes/js/main.js
+++ nahes/js/main.js
@@ -21,4 +21,30 @@ $(document).ready(function(){
 
     });
 
+    let pageWrapper = document.querySelector(".collection");
+    let items = document.querySelector(".pf");
+    let localItems = gsap.utils.toArray(".pf");
+
+    let mm = gsap.matchMedia();
+
+    let distance = () => {
+        return items.scrollWidth - window.innerWidth;
+    };
+
+    mm.add("(min-width: 1025px)", () => {
+        gsap.to(localItems, {
+            x: () => -distance(),
+            ease: "none",
+            scrollTrigger: {
+                trigger: pageWrapper,
+                start: "top top",
+                pinnedContainer: pageWrapper,
+                pin: pageWrapper,
+                end: () => "+=" + distance(),
+                scrub: true,
+                invalidateOnRefresh: true
+            }
+        })
+    })
+
 });
